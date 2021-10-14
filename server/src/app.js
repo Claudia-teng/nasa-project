a4f4354fdba4c5483d6b3c46fde1b4b1fbf7cbee
Claudia Teng <sydeng102@gmail.com>
2021-10-14 20:58:13 +0800

    add cors & loading data from startup

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const planetsRouter = require('./routes/planet/planet.routers');
 
 const app = express();
+app.use(cors({
+  origin: 'http://localhost:3000'
+}));
 app.use(express.json());
 app.use('/planets', planetsRouter);
 
