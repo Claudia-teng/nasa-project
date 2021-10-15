3d1b564dbf73a7428ac864d364f4ce06f8d93b36
Claudia Teng <sydeng102@gmail.com>
2021-10-15 18:39:11 +0800

    add post launch api

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,11 +1,13 @@
 const launches = new Map();
 
+let latestFlightNumber = 100;
+
 const launch = {
   flightNumber: 100,
   mission: 'Kepler Exploration X',
   rocket: 'Explorer IS1',
-  lanuchData: new Date('December 27, 2030'),
-  destination: 'Kepler-442 b',
+  launchData: new Date('December 27, 2030'),
+  target: 'Kepler-442 b',
   customer: ['ZTM', 'NASA',],
   upcoming: true,
   success: true
@@ -17,6 +19,19 @@ const getAllLaunches = () => {
   return Array.from(launches.values());
 }
 
+const addNewLaunch = (launch) => {
+  latestFlightNumber++;
+  launches.set(
+    latestFlightNumber,
+    Object.assign(launch, {
+      success: true,
+      upcoming: true,
+      customers: ['Zero to Mastery', 'NASA'],
+      flightNumber: latestFlightNumber
+    }))
+}
+
 module.exports = {
-  getAllLaunches
+  getAllLaunches,
+  addNewLaunch
 }
